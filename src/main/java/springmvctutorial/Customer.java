469a01624e27c2da8614aad4e9baaaace9c20fa9
kusaldushmantha <kusald.14@cse.mrt.ac.lk>
2020-04-05 10:37:55 +0530

    Loading custom messages from a resource file

--- src/main/java/springmvctutorial/Customer.java
+++ src/main/java/springmvctutorial/Customer.java
@@ -1,5 +1,7 @@
 package springmvctutorial;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -14,6 +16,11 @@ public class Customer
     @NotNull( message = "is required" )
     @Size( min = 1, message = "is required" )
     private String lastName;
+
+    @NotNull( message = "is required" )
+    @Min( value = 0, message = "must be greater than 0" )
+    @Max( value = 10, message = "must be lesser than 10" )
+    private Integer freePasses;
 
     public String getFirstName()
     {
@@ -33,5 +40,15 @@ public class Customer
     public void setLastName( String lastName )
     {
         this.lastName = lastName;
+    }
+
+    public Integer getFreePasses()
+    {
+        return freePasses;
+    }
+
+    public void setFreePasses( Integer freePasses )
+    {
+        this.freePasses = freePasses;
     }
 }
--- src/main/webapp/WEB-INF/view/customer-form.jsp
+++ src/main/webapp/WEB-INF/view/customer-form.jsp
@@ -20,6 +20,10 @@
         Last Name(*) : <form:input path="lastName"/>
         <form:errors path="lastName" cssClass="error"/>
         <br><br>
+        Free passes(*) : <form:input path="freePasses"/>
+        <form:errors path="freePasses" cssClass="error"/>
+        <br><br>
+
         <input type="submit" value="submit"/>
     </form:form>
 </body>
